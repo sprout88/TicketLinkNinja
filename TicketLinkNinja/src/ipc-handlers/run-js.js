13b5b16a3ec5d5517f3c82fc64c0f27d26edf558
Jaeho Jeon <88sprout@gmail.com>
2025-06-23 18:18:45 +0900

    feat: 스크립트 성공 여부 매핑하여 반환

--- TicketLinkNinja/src/ipc-handlers/run-js.js
+++ TicketLinkNinja/src/ipc-handlers/run-js.js
@@ -1,6 +1,11 @@
 function runJs(win, script) {
   if (win && !win.isDestroyed()) {
-    return win.webContents.executeJavaScript(script);
+    return win.webContents.executeJavaScript(script).then(result => {
+      if (typeof result === 'undefined') {
+        return '[RESULT] success (no return value)';
+      }
+      return `[RESULT] ${result}`;
+    });
   }
   return Promise.resolve('[SKIPPED] no window');
 }
